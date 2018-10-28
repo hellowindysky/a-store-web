dfa181ad1a874db406821d8e59488b669dc6a9bf
zhangquan <quanys09@qq.com>
2018-10-28 20:32:51 +0800

    modify vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 // vue.config.js
+const path = require('path')
+
 module.exports = {
-  // 选项...
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+    } else {
+      // 为开发环境修改配置...
+    }
+    // 开发生产共同配置
+    config.resolve.alias['components'] = path.resolve(__dirname, './src/components')
+    config.resolve.alias['views'] = path.resolve(__dirname, './src/views')
+  },
+  devServer: {
+    open: false,
+    host: '0.0.0.0',
+    port: 8085
+  }
 }
